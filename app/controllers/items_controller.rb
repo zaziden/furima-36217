3f2266214a297aab30e414a5e51bdd51b9fe339c
zaziden <n.n1013n.n@icloud.com>
2021-08-02 18:05:06 +0900

    商品一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    #@items = Item.order("created_at DESC")
+    @items = Item.order("created_at DESC")
     #@items = Item.new
   end
 
