ca187b582d731db0daadc4a1dd3a4f859c90b508
zaziden <n.n1013n.n@icloud.com>
2021-10-15 16:29:16 +0900

    修正

--- Capfile
+++ Capfile
@@ -5,5 +5,5 @@ require 'capistrano/bundler'
 require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
 require 'capistrano3/unicorn'
-
+install_plugin Capistrano::SCM::Git
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'furima-36217'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'zaziden/furima-36217.git'
+set :repo_url,  'git@github.com:zaziden/furima-36217.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
